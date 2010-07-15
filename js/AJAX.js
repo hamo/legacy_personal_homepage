5182b02dacab7fc2e259721dc0cf5e17717e8a72
Yang Hamo Bai <hamo.by@gmail.com>
2010-07-16 07:20:35 +0800

    remove debug information

--- js/AJAX.js
+++ js/AJAX.js
@@ -1,52 +1,50 @@
 function GetXmlHttpObject()
 {
-  var xmlHttp=null;
-  try
-    {
-    // Firefox, Opera 8.0+, Safari
-    xmlHttp=new XMLHttpRequest();
-    }
-  catch (e)
-    {
-    // Internet Explorer
+    var xmlHttp=null;
     try
-      {
-      xmlHttp=new ActiveXObject("Msxml2.XMLHTTP");
-      }
-    catch (e)
-      {
-      xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
-      }
+    {
+        // Firefox, Opera 8.0+, Safari
+        xmlHttp=new XMLHttpRequest();
     }
-  return xmlHttp;
+    catch (e)
+    {
+        // Internet Explorer
+        try
+        {
+            xmlHttp=new ActiveXObject("Msxml2.XMLHTTP");
+        }
+        catch (e)
+        {
+            xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+        }
+    }
+    return xmlHttp;
 }
 
 function ChangeContent(str)
 {
 
-  xmlHttp=GetXmlHttpObject()
-  
-  if (xmlHttp==null)
-    {
-    alert ("您的浏览器不支持AJAX！");
-    return;
-    }
+    xmlHttp=GetXmlHttpObject()
 
-var url;
-url="./"+str+".html";
-xmlHttp.onreadystatechange=stateChanged;
-xmlHttp.open("GET",url,true);
-xmlHttp.send(null);
+        if (xmlHttp==null)
+        {
+            alert ("We need AJAX to run!");
+            return;
+        }
+
+    var url;
+    url=str+".html";
+    xmlHttp.onreadystatechange=stateChanged;
+    xmlHttp.open("GET",url,true);
+    xmlHttp.send(null);
 }
 
 function stateChanged() 
 { 
-  if (xmlHttp.readyState==4)
-  { 
-      if (xmlHttp.status=200){
-
-      alert(xmlHttp.responseText);
-  document.getElementById("content").innerHTML=xmlHttp.responseText;
-      }
-  }
+    if (xmlHttp.readyState==4)
+    { 
+        if (xmlHttp.status=200){
+            document.getElementById("content").innerHTML=xmlHttp.responseText;
+        }
+    }
 }
