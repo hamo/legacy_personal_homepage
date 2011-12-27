59cea378871b37dc26787b3b9de8d0bbca3cab6b
Yang Bai <hamo.by@gmail.com>
2011-12-27 10:20:14 +0800

    Revert "Compress JS file AJAX.js"
    This reverts commit 4bbd977f93328c7ee49ad3bcc8b8ca2da4a44af0.

--- js/AJAX.js
+++ js/AJAX.js
@@ -1,1 +1,60 @@
-function GetXmlHttpObject(){var a=null;try{a=new XMLHttpRequest()}catch(e){try{a=new ActiveXObject("Msxml2.XMLHTTP")}catch(e){a=new ActiveXObject("Microsoft.XMLHTTP")}}return a}function ChangeContent(a){xmlHttp=GetXmlHttpObject()if(xmlHttp==null){alert("We need AJAX to run!");return}var b;b=a+".html";xmlHttp.onreadystatechange=stateChanged;xmlHttp.open("GET",b,true);xmlHttp.send(null);if(a=="powered_by"){document.getElementById("content").style.width="100%"}else{document.getElementById("content").style.width="80%"}}function stateChanged(){if(xmlHttp.readyState==4){if(xmlHttp.status==200){document.getElementById("content").innerHTML=xmlHttp.responseText}}else{document.getElementById("content").innerHTML="<div style='text-align: center;'><img src='ajax-loader.gif' alt='' /></div>"}}
+function GetXmlHttpObject()
+{
+    var xmlHttp=null;
+    try
+    {
+        // Firefox, Opera 8.0+, Safari
+        xmlHttp=new XMLHttpRequest();
+    }
+    catch (e)
+    {
+        // Internet Explorer
+        try
+        {
+            xmlHttp=new ActiveXObject("Msxml2.XMLHTTP");
+        }
+        catch (e)
+        {
+            xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+        }
+    }
+    return xmlHttp;
+}
+
+function ChangeContent(str)
+{
+
+    xmlHttp=GetXmlHttpObject()
+
+        if (xmlHttp==null)
+        {
+            alert ("We need AJAX to run!");
+            return;
+        }
+
+    var url;
+    url=str+".html";
+    xmlHttp.onreadystatechange=stateChanged;
+    xmlHttp.open("GET",url,true);
+    xmlHttp.send(null);
+
+    if (str == "powered_by"){
+        document.getElementById("content").style.width="100%";
+    }
+    else{
+        document.getElementById("content").style.width="80%";
+    }
+}
+
+function stateChanged() 
+{ 
+    if (xmlHttp.readyState==4)
+    { 
+        if (xmlHttp.status==200){
+            document.getElementById("content").innerHTML=xmlHttp.responseText;
+        }
+    }
+    else{
+        document.getElementById("content").innerHTML="<div style='text-align: center;'><img src='ajax-loader.gif' alt='' /></div>";
+    }
+}
